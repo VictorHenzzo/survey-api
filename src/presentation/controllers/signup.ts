7cafc0f5088187056a007cf92d1ccac6d1d9ea62
Victor <victorhenzzowp@gmail.com>
2024-04-27 16:06:17 -0300

    chore: make SignUpController implements Controller protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import BadRequest from "../errors/http_error";
 import { MissingParamError } from "../errors/missing_param_error";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { Controller } from "../protocols/controller";
 
-export class SignUpController {
+export class SignUpController implements Controller {
   constructor() {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
